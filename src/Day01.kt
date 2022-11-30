face55f8f095538dbb7fb8637bf0024a4c5fce34
ahatfield <andrewscotthat@gmail.com>
2022-11-29 23:56:15 -0800

    Sets up some initial utilities: * Output - print to console and copy to clipboard. * Updates readInput to create a file if it doesn't exist. * Creates tryGetInputFromSite which makes a request to Advent of Code to get the puzzle input.
    Also adds coroutines as a dependency for convenience if we want to use them later.

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,17 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
+  fun part1(input: List<String>): Int {
+    return input.size
+  }
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
+  fun part2(input: List<String>): Int {
+    return input.size
+  }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+  // test if implementation meets criteria from the description, like:
+  val testInput = readInput(1, test=true)
+  output(part1(testInput), "test")
 
-    val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+  val input = readInput(1)
+  output(part1(input), "pt1")
+  output(part2(input), "pt2")
 }
--- src/Utils.kt
+++ src/Utils.kt
@@ -1,13 +1,54 @@
+import java.awt.datatransfer.StringSelection
+import java.awt.Toolkit
 import java.io.File
 import java.math.BigInteger
+import java.net.URI
+import java.net.http.HttpClient
+import java.net.http.HttpRequest
+import java.net.http.HttpResponse
 import java.security.MessageDigest
+
+const val year = 2022
+
 
 /**
  * Reads lines from the given input txt file.
  */
-fun readInput(name: String) = File("src", "$name.txt").readLines()
+fun readInput(dayNum: Int, test: Boolean = false): List<String> {
+    val paddedDay = dayNum.toString().padStart(2, '0')
+    val fileName = if (test) "Day${paddedDay}_test.txt" else "Day${paddedDay}.txt"
+    val file = File("src", "$fileName.txt")
+
+    if (!file.exists()) {
+        file.writeText(if (test) "" else tryGetInputFromSite(dayNum))
+    }
+
+    return file.readLines()
+}
+
+fun tryGetInputFromSite(day: Int): String {
+    val client = HttpClient.newBuilder().build()
+    val request = HttpRequest.newBuilder()
+        .uri(URI.create("https://adventofcode.com/$year/day/$day/input"))
+        .setHeader("cookie", File("src", ".cookie").readText())
+        .build()
+
+    val response = client.send(request, HttpResponse.BodyHandlers.ofString())
+    return response.body()
+}
 
 /**
  * Converts string to md5 hash.
  */
 fun String.md5(): String = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray())).toString(16)
+
+fun output(o: Any, prefix: String = "") {
+    println("$prefix $o")
+
+    Toolkit.getDefaultToolkit()
+        .getSystemClipboard()
+        .setContents(
+            StringSelection(o.toString()),
+            null
+        )
+}
