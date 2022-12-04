9a75ab4ad8d0118c5b678248292ecb0682e2075c
ahatfield <andrewscotthat@gmail.com>
2022-12-03 16:10:39 -0800

    Updates part 2 to use reduce + intersect.

--- src/Day03.kt
+++ src/Day03.kt
@@ -1,14 +1,35 @@
 private class Day03(lines: List<String>) {
-  init { println(lines) }
+  init {
+    println(lines)
+  }
 
   private val lines = lines
+    .map {
+      val s = it.length / 2
+      listOf(it.toList().subList(0, s), it.toList().subList(s, it.length))
+    }.map {
+      it[0].intersect(it[1]).first()
+    }.map {
+      score(it)
+    }
+
+  private val lines2 = lines.windowed(3, step = 3)
+    .map {
+      it.map { it.toMutableList() }.reduce { acc, chars -> acc.intersect(chars).toMutableList() }.first()
+    }.map {
+      score(it)
+    }
+
+  fun score(it: Char): Int {
+    return if (it.isUpperCase()) it.code - 'A'.code + 27 else it.code - 'a'.code + 1
+  }
 
   fun part1(): Int {
-    return lines.size
+    return lines.sum()
   }
 
   fun part2(): Int {
-    return lines.size
+    return lines2.sum()
   }
 }
 
