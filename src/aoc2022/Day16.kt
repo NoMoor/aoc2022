67e2e8e6c92a3599b12ce9239c1d1049d0d79109
ahatfield <andrewscotthat@gmail.com>
2022-12-16 08:14:21 -0800

    Day 16 Solved - Refactor.

--- src/aoc2022/Day16.kt
+++ src/aoc2022/Day16.kt
@@ -13,24 +13,25 @@ private class Day16(val lines: List<String>) {
 
   init {
     val denseEdges = mutableMapOf<String, List<Edge>>()
-    val denseRooms = lines.map {
-      val (a, b) = it.split(";")
+    val denseRooms = lines.map { l ->
+      val (a, b) = l.split(";")
       val name = a.split(" ")[1]
       val flow = a.split("=")[1].toLong()
-      val edges = b.split("valve")[1]
+
+      denseEdges[name] = b.split("valve")[1]
         .removePrefix("s")
         .removePrefix(" ")
         .split(", ")
         .map { Edge(it, 1) }
 
-      denseEdges.put(name, edges)
-
       Room(name, flow)
     }
 
-    roomMap = denseRooms.filter { it.rate >= 0 || it.name == STARTING_ROOM }.associateBy { it.name }
+    roomMap = denseRooms
+      .filter { it.rate >= 0 || it.name == STARTING_ROOM }
+      .associateBy { it.name }
 
-    // Make paths from every room to every other room
+    // Make shortest paths from every room to every other room
     sparseEdges = denseRooms.associate { r ->
       val shortestPath = computeSparseEdges(r, denseEdges)
 
@@ -41,32 +42,32 @@ private class Day16(val lines: List<String>) {
     }
   }
 
+  /** Create min spanning tree for this room. */
   private fun computeSparseEdges(r: Room, denseEdges: Map<String, List<Edge>>): Map<String, Int> {
-    // Create spanning tree for this room
-    val shortestPath = mutableMapOf<String, Int>()
-    val stack = PriorityQueue<Pair<String, Int>>(compareBy { it.second }) // PQ
-    stack.add(r.name to 0)
+    val shortestPaths = mutableMapOf<String, Int>()
+    val pq = PriorityQueue<Pair<String, Int>>(compareBy { it.second }) // PQ
+    pq.add(r.name to 0)
 
-    while (stack.isNotEmpty()) {
-      val (name, distance) = stack.poll()
+    while (pq.isNotEmpty()) {
+      val (name, distance) = pq.poll()
 
       // check
-      if (shortestPath.containsKey(name) && shortestPath[name]!! <= distance) {
+      if (shortestPaths.containsKey(name) && shortestPaths[name]!! <= distance) {
         continue
       }
-      shortestPath[name] = distance
+      shortestPaths[name] = distance
 
-      stack.addAll(denseEdges[name]!!.map { it.dest to distance + 1 })
+      pq.addAll(denseEdges[name]!!.map { it.dest to distance + 1 })
     }
-    return shortestPath
+    return shortestPaths
   }
 
   data class Room(val name: String, val rate: Long)
   data class Edge(val dest: String, val weight: Int)
+  data class Actor(val minute: Int, val location: String)
 
   data class State(
-    val minute: Int,
-    val location: Room,
+    val actor: Actor,
     val openValves: Set<String>,
     val isEle: Boolean = false,
     val isPt2: Boolean = false
@@ -74,14 +75,14 @@ private class Day16(val lines: List<String>) {
 
   fun part1(): Long {
     cache.clear()
-    val state = State(0, roomMap.get(STARTING_ROOM)!!, setOf())
-    return sparseEdges[state.location.name]!!.maxOfOrNull { goto(it, state, 30) }!!
+    val state = State(Actor(0, STARTING_ROOM), setOf())
+    return sparseEdges[state.actor.location]!!.maxOfOrNull { goto(it, state, 30) }!!
   }
 
   fun part2(): Long {
     cache.clear()
-    val state = State(0, roomMap[STARTING_ROOM]!!, setOf(), isPt2 = true)
-    return sparseEdges[state.location.name]!!.maxOfOrNull { goto(it, state, 26) }!!
+    val state = State(Actor(0, STARTING_ROOM), setOf(), isPt2 = true)
+    return sparseEdges[state.actor.location]!!.maxOfOrNull { goto(it, state, 26) }!!
   }
 
   /**
@@ -96,12 +97,12 @@ private class Day16(val lines: List<String>) {
     // Put us on turn + traversal time + 1 turn to turn on the valve.
     // We can check the turn to change the valve now because if we don't have time to turn it, we can't score
     // any more points.
-    val currentMinute = prevState.minute + edge.weight + 1
+    val currentMinute = prevState.actor.minute + edge.weight + 1
 
     if (currentMinute >= totalMinutes && prevState.isPt2 && !prevState.isEle) {
       // Once we've taken all our turns, Let the elephant take all its turns.
-      val eleState = prevState.copy(minute = 0, location = roomMap[STARTING_ROOM]!!, isEle = true)
-      val max = sparseEdges[eleState.location.name]!!.filter { it.dest !in prevState.openValves }
+      val eleState = prevState.copy(actor = Actor(0, STARTING_ROOM), isEle = true)
+      val max = sparseEdges[eleState.actor.location]!!.filter { it.dest !in prevState.openValves }
         .map { goto(it, eleState, totalMinutes) }
         .max()
 
@@ -114,13 +115,13 @@ private class Day16(val lines: List<String>) {
     val openValves = buildSet { addAll(prevState.openValves); add(loc.name) }
     val pressureReleasedThisTurn = loc.rate * (totalMinutes - currentMinute)
 
-    val newState = prevState.copy(minute = currentMinute, location = loc, openValves=openValves)
+    val newState = prevState.copy(actor = Actor(currentMinute, loc.name), openValves=openValves)
 
     if (cache.containsKey(newState)) {
       return cache[newState]!!
     }
 
-    val maxPressureReleasedAfterThisTurn = sparseEdges[newState.location.name]!!
+    val maxPressureReleasedAfterThisTurn = sparseEdges[newState.actor.location]!!
       .filter { !newState.openValves.contains(it.dest) }
       .map { goto(it, newState, totalMinutes) }
       .maxOrNull() ?: 0L
